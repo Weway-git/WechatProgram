56f4c92de9e8f69f4acf2fe1ff4da80252d6223e
chenfu <1649226149@qq.com>
2020-02-11 18:17:24 +0800

    分类选择导航块

--- DG商城/pages/index/index.js
+++ DG商城/pages/index/index.js
@@ -1,38 +1,47 @@
 //Page Object
+import{request} from "../../request/index.js";
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList:[],
+    // 导航数组
+    catesList:[]
   },
   //options(Object)
-  onLoad: function(options){
-    
-  },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
+  onLoad: function (options) {
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result)=>{
+    //     // console.log(result);
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // });
 
+    // request({ url: "https://api.zbztb.cn/api/public/v1/home/swiperdata" })
+    //   .then(result =>
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     }))
+    // // .then()
+    this.getSwiperList();
+    this.getCatesList();
   },
-  onUnload: function(){
-
+  // 获取轮播图数据
+  getSwiperList(){
+    request({ url: "https://api.zbztb.cn/api/public/v1/home/swiperdata" })
+    .then(result =>
+      this.setData({
+        swiperList: result.data.message
+      }))
   },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
+  // 获取导航数组数据
+  getCatesList(){
+    request({ url: "https://api.zbztb.cn/api/public/v1/home/catitems" })
+    .then(result =>
+      this.setData({
+        catesList: result.data.message
+      }))
   }
 });
