2c23b1162a2fa84a0b0fcce79cac4448c8929b65
Weway-git <1419864987@qq.com>
2020-02-28 15:45:07 +0800

    客服

--- DG商城/pages/goods_detail/index.js
+++ DG商城/pages/goods_detail/index.js
@@ -1,66 +1,53 @@
 // pages/goods_detail/index.js
+import { request } from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    goodsObj:{}
 
   },
+  // 商品对象
+  GoodsInfo:{},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const {goods_id}=options;
+    console.log(goods_id)
+    this.getGoodsDetail(goods_id) 
 
   },
+  // 获取商品详情
+  async  getGoodsDetail(goods_id){
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      const goodsObj=await request({url:"/goods/detail",data:{goods_id}});
+      this.GoodsInfo=goodsObj;
+      console.log(goodsObj)
+      this.setData({
+        goodsObj:{
+          goods_name:goodsObj.goods_name,
+          goods_price:goodsObj.goods_price,
+          goods_introduce:goodsObj.goods_introduce,
+          pics:goodsObj.pics
+        }
+      })
+      },
+  // 点击轮播图方法预览
+  handlePreviewImage(e){
+    // 构造预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v=>v.pics_mid);
+    // 接受传递过来的图片url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current, // 当前显示图片的http链接
+      urls, // 需要预览的图片http链接列表
+    })
   }
+  
+
+  
 })
